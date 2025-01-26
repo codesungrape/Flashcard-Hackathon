e3d25da39648444e4ddbd7a7a7694ce6d632f777
Shanti Rai <shantirai@MacBookAir.lan>
2025-01-26 18:36:37 +0000

    moved toggle logic to parent container as it is soemthing that will determine which questions go into each container.

--- src/components/Flashcard/Flashcard.jsx
+++ src/components/Flashcard/Flashcard.jsx
@@ -1,19 +1,9 @@
-import { useState } from 'react'
+
 import styles from './Flashcard.module.css'
 
-export default function Flashcard() {
+export default function Flashcard({cardSide, handleClick}) {
 
-    // make the form change state on CLick- DONE
-    // make the form toggle from 'Question' to 'Answer'
-
-
-    // set up useState
-    const [cardSide, setCardSide] = useState("Question")
-
-    function handleClick() {
-        setCardSide(prev => prev === "Question" ? "Answer" : "Question")
-    }
-
+    
     return (
         <section className={styles.flashcard} onClick={handleClick} >
             <p>{cardSide}</p>
--- src/components/FlashcardContainer/FlashcardContainer.jsx
+++ src/components/FlashcardContainer/FlashcardContainer.jsx
@@ -1,12 +1,25 @@
 import Flashcard from '../Flashcard/Flashcard'
-
+import { useState } from 'react'
 export default function FlashcardContainer() {
+
+    const [cardSide, setCardSide] = useState("Question")
+
+    function handleClick() {
+        setCardSide(prev => prev === "Question" ? "Answer" : "Question")
+    }
+
     return (
         <section style={{ border: '2px solid black', padding: '20px', borderRadius: '8px' }}>
         {/* Flashcards content will go here */}
-            <Flashcard />
-            <Flashcard />
-            <Flashcard />
+            <Flashcard cardSide={cardSide} handleClick={handleClick}/>
+            <Flashcard cardSide={cardSide} handleClick={handleClick}/>
+            <Flashcard cardSide={cardSide} handleClick={handleClick}/>
         </section>
     )
 }
+
+
+
+
+
+ // now that ive set up the toggle, how do I push new data to each Flashcard? 
